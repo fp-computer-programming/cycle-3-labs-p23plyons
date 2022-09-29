68196bd9f2ca885594d4de5b1e1588a37d306da4
p23pylons <p23plyons@fairfieldprep.org>
2022-09-29 12:04:41 -0400

    Submission of cycle 3 day 4 lab

---
+++ lab_3-4.py
@@ -0,0 +1,33 @@
+# Author: PCL 09/29/22
+
+import math
+
+#1: predicted answer = 8
+print(math.ceil(7.15))
+
+#2: predicted answer = 19
+print(math.floor(19.156))
+
+#3: predicted answer = 42
+print(math.fabs(-42))
+
+#4: predicted answer = 8
+print(math.pow(2,3))
+
+#5: predicted answer = 2
+print(math.log10(100))
+
+#6: predicted answer = 3
+print(math.sqrt(9))
+
+#7: predicted answer = 1
+print(math.isqrt(2))
+
+#8: predicted answer = 120
+print(math.factorial(5))
+
+#9: predicted answer = 333
+print(math.gcd(111111111, 12321))
+
+#10: predicted answer = 0
+print(math.sin(0))
