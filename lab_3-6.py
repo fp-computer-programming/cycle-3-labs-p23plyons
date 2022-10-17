b823972e6c03385761ca5a333950f845161522d7
p23pylons <p23plyons@fairfieldprep.org>
2022-10-17 13:43:49 -0400

    Submission of cycle 3 day 6 lab
    I realize that I forgot to submit this

---
+++ lab_3-6.py
@@ -0,0 +1,17 @@
+# Author: PCL 10/3/22
+
+from multiprocessing.pool import ApplyResult
+
+
+school = 'Fairfield ' + 'Prep'
+print(school)
+
+line = '-' * 20
+print(line)
+
+print('I\'m loving this short story I\'m reading, \"The Fall of the House of Usher\"')
+
+print(len('I\'m loving this short story I\'m reading, \"The Fall of the House of Usher\"'))
+
+x = 'apple'
+print(x[3])
